d5c0a46bf79949b1b89fc772a5504854f776b48b
tnraro <deokji53@gmail.com>
2024-02-07 14:57:26 +0900

    feat(emoji): add emoji

---
+++ emoji/src/index.ts
@@ -0,0 +1,5 @@
+import data0 from "cldr-annotations-derived-modern/annotationsDerived/ko/annotations.json";
+import data1 from "cldr-annotations-modern/annotations/ko/annotations.json";
+
+console.log(data0.annotationsDerived.identity)
+console.log(data1.annotations.identity)
